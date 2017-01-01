4abeacff8a7331512e9177d446d8d75730b6fb7d
vabarca <viabfer@gmail.com>
2017-01-01 21:02:25 +0100

    Update project

--- DDs/src/DDs.ino
+++ DDs/src/DDs.ino
@@ -1,9 +1,7 @@
-#include <Arduino.h>
-
-/* Random Dogs Barking
- * a direct-digital synthesis "tutorial"
- * for the AVR, but will compile on an Arduino
- * Released public domain, because it's absurd, frankly.
+/* Simple Sinewave DDS Routine
+ * Speedy version -- using bit-shifts in the accumulator.
+ * See https://github.com/hexagon5un/barking_dogs_dds_demo
+ * Public Domain
  */
 
 #include <avr/interrupt.h>
@@ -13,77 +11,56 @@
 #include "scale16.h"
 #include "sineWav.h"
 
-#define clear_bit(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#define set_bit(sfr, bit)   (_SFR_BYTE(sfr) |= _BV(bit))
-#define LED_ON              set_bit(PORTB, PB5) // debug, pin PB1, Arduino D9
-#define LED_OFF             clear_bit(PORTB, PB5)
-#define LED_INIT            set_bit(DDRB, PB5) /* debugging -- this pin toggled in ISR  */
+#define clear_bit(sfr, bit)   (_SFR_BYTE(sfr) &= ~_BV(bit))
+#define set_bit(sfr, bit)     (_SFR_BYTE(sfr) |= _BV(bit))
+#define toggle_bit(sfr, bit)  (_SFR_BYTE(sfr) ^= _BV(bit))
 
-  /* Note: If ACCUMULATOR_STEPS is a power of two,
-   * the math works fast and all is well.
-   * If not, the division and modulo by ACCUMULATOR_STEPS kills it.
-   * */
+#define SAMPLE_RATE       24000UL
 #define ACCUMULATOR_STEPS 2048
-#define SAMPLE_RATE       8000UL
-#define NUM_TONES         1
 
 /* Init functions, defined at the bottom of the file */
 static inline void setup_pwm_audio_timer(void);
 static inline void setup_sample_timer(void);
-static inline void init_ouput_pins(void);
 
-const uint16_t ui16SineLUT_size = sizeof(ui8SineLUT);
-
-struct structTone
-{
+struct DDS {
   uint16_t increment = ACCUMULATOR_STEPS;
   uint16_t position = 0;
   uint16_t accumulator = 0;
 };
-volatile struct structTone Tone;
+volatile struct DDS sineTone;
+
+const uint16_t sineTone_size = sizeof(ui8SineLUT);
 
 ISR(TIMER1_COMPA_vect)
 {
-  LED_ON; // debug, toggle pin
+  PORTB |= (1 << PB1); // debug, toggle pin PB1, Arduino D9
 
-  OCR2A = (int8_t)pgm_read_byte_near(ui8SineLUT + Tone.position);
+  OCR2A = (int8_t)pgm_read_byte_near(ui8SineLUT + sineTone.position);
 
-  if (Tone.position < ui16SineLUT_size)
+  if (sineTone.position < sineTone_size)
   {    /* playing */
-    Tone.accumulator += Tone.increment;
-    Tone.position += Tone.accumulator / ACCUMULATOR_STEPS;
-    Tone.accumulator = Tone.accumulator % ACCUMULATOR_STEPS;
-  }
-  else
+      sineTone.accumulator += sineTone.increment;
+      sineTone.position += sineTone.accumulator / ACCUMULATOR_STEPS;
+      sineTone.accumulator = sineTone.accumulator % ACCUMULATOR_STEPS;
+  } else
   {  /*  done playing, reset and wait  */
-    Tone.position = 0;
-    Tone.increment = 0;
+      sineTone.position = 0;
   }
 
-  LED_OFF; // debug, toggle pin
+  PORTB &= ~(1 << PB1); // debug, toggle pin
 }
-
-// These constants are defined in "scale16.h"
-uint16_t scale[] = {NOTE_C1, NOTE_E1, NOTE_G1, NOTE_C2, NOTE_E2, NOTE_G2, NOTE_C3};
-const uint8_t scale_max = sizeof(scale)/sizeof(scale[0]);
 
 void setup()
 {
   setup_sample_timer();
   setup_pwm_audio_timer();
+  set_bit(DDRB, PB1); // debugging
 
-  LED_INIT;
+  sineTone.increment = 1;
 }
 
 void loop()
 {
-  /* Demo the entire scale */
-  for (uint8_t i=0; i < scale_max; i++)
-  {
-    Tone.increment = scale[i];
-    set_bit(PORTC, PC0);   /* Turn on LED for light show. */
-    _delay_ms(2000);
-  }
 }
 
 
